ec7a7b19efa78aa08f61d9f720923fc4b7a2b569
Jamie Francisco <0jamiefrancisco@gmail.com>
2023-10-13 17:01:54 -0700

    Complete book file passing all tests

--- src/book.js
+++ src/book.js
@@ -1,12 +1,45 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return modifiedTitle = `The ${title}`;
+}
+
+function buildMainCharacter(characterName, characterAge, characterPronouns) {
+  return character = {
+    name: characterName,
+    age: characterAge,
+    pronouns: characterPronouns
+  }
+}
+function saveReview(newReview, reviews) {
+  if (reviews.includes(newReview)) {
+    return reviews;
+  } else {
+    return reviews.push(newReview);
+  }
+}
+
+function calculatePageCount(bookTitle) {
+// console.log(bookTitle.length);
+  return bookPageCount = (bookTitle.length) * 20;
+}
+
+function writeBook(bookTitle, bookCharacter, bookGenre) {
+  return book = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: bookGenre
+  };
+}
+
+function editBook(book) {
+  return book.pageCount = (book.pageCount) * .75
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
